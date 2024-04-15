f4179844e4ef67b1f187f214d475368816da8fb6
Arkadiy <neykerez@gmail.com>
2024-04-15 14:30:16 +0300

    add in function test_masks.py @pytest.fixture and @pytest.mark.parametrize

--- tests/tests_masks.py
+++ tests/tests_masks.py
@@ -1,18 +1,32 @@
 import pytest
+
 from src.masks import mask_account_number, mask_credit_card_number
 
 
-def test_mask_card_or_account():
-    assert mask_credit_card_number('7000792289606361') == '7000 79** **** 6361'
-    assert mask_credit_card_number('1234567890123456') == '1234 56** **** 3456'
+@pytest.fixture
+def card_numbers() -> list:
+    return [("7000792289606361", "7000 79** **** 6361"), ("1234567890123456", "1234 56** **** 3456")]
 
 
-def test_form_mask_credit_card_number():
-    assert mask_account_number('73654108430135874305') == '**4305'
-    assert mask_account_number('1234567890123456') == '**3456'
+@pytest.fixture
+def account_numbers() -> list:
+    return [("73654108430135874305", "**4305"), ("1234567890123456", "**3456")]
+
+
+@pytest.mark.parametrize("original_number, masked_number", [("7000792289606361", "7000 79** **** 6361"),
+                                                            ("1234567890123456", "1234 56** **** 3456")])
+def test_mask_card_or_account(original_number, masked_number):
+    assert mask_credit_card_number(original_number) == masked_number
+
+
+@pytest.mark.parametrize("original_number, masked_number", [("73654108430135874305", "**4305"), ("1234567890123456", "**3456")])
+def test_form_mask_credit_card_number(original_number, masked_number):
+    assert mask_account_number(original_number) == masked_number
 
 
 if __name__ == "__main__":
     pytest.main()
+    test_mask_card_or_account()
+    test_form_mask_credit_card_number()
 
-print('Всё выводит ты красавчик')
+print("Всё выводит ты красавчик")
