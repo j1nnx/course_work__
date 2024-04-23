0b5eacceec8da6be632e381431c39eb97bdf01f2
Arkadiy <neykerez@gmail.com>
2024-04-23 15:53:54 +0300

    update module decorators.py and file mylog.txt

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,24 @@
-from functools import wraps
 import datetime
+from functools import wraps
+from typing import Any, Callable, Optional, Tuple
 
 
-def log(filename=None):
-    def decorator(func):
+def log(filename: Optional[str] = None) -> Callable[[Callable], Callable]:
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Tuple[Any], **kwargs: Any) -> Any:
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_message = f'{timestamp} {func.__name__}'
+            log_message = f"{timestamp} {func.__name__}"
 
             try:
                 result = func(*args, **kwargs)
-                log_message += 'ok \n'
+                log_message += " ok\n"
             except Exception as e:
-                log_message += f'error: {type(e).__name__}. Inputs: {args} {kwargs}\n'
+                log_message += f"error: {type(e).__name__}. Inputs: {args} {kwargs}\n"
                 result = None
 
             if filename:
-                with open(filename, 'a') as f:
+                with open(filename, "a") as f:
                     f.write(log_message)
             else:
                 print(log_message)
@@ -25,15 +26,5 @@ def log(filename=None):
             return result
         return wrapper
     return decorator
-
-
-@log(filename="mylog.txt")
-def my_function(x, y):
-    return x + y
-
-my_function(1, 2)
-
-
-
 
 
