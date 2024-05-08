3a0d56b04453a1991582eab3f317960572b09cb5
Arkadiy <neykerez@gmail.com>
2024-05-08 16:53:34 +0300

    recoomit

--- src/utils.py
+++ src/utils.py
@@ -27,13 +27,13 @@ def read_transaction_from_file_json(file_path: str) -> List[Dict[str, Union[str,
 
 def get_transactions_rub_to_usd(currency: str) -> Any:
     """Получает курс валюты в рублях по отношению к USD и EUR"""
-    url = f'https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}'
-    headers = {'apikey': API_KEY}
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
+    headers = {"apikey": API_KEY}
     try:
         response = requests.get(url, headers=headers, timeout=5)
         response.raise_for_status()
         response_data = response.json()
-        return response_data['rates']['RUB']
+        return response_data["rates"]["RUB"]
     except requests.exceptions.RequestException as ef:
         print(ef)
         return 1.0
@@ -43,14 +43,12 @@ def sum_amount_from_transactions(transactions: List[Dict]) -> float:
     """Returns the sum if transactions in Rub"""
     total = 0.0
     for transact in transactions:
-        operation_sum = transact.get('operationAmount', {})
-        currency_code = operation_sum.get('currency', {}).get('code', '')
-        amount = float(operation_sum.get('amount', 0.0))
-        if currency_code in ['USD', 'EUR']:
+        operation_sum = transact.get("operationAmount", {})
+        currency_code = operation_sum.get("currency", {}).get("code", "")
+        amount = float(operation_sum.get("amount", 0.0))
+        if currency_code in ["USD", "EUR"]:
             rate_to_rub = get_transactions_rub_to_usd(currency_code)
             total += amount * rate_to_rub
-        elif currency_code == 'RUB':
+        elif currency_code == "RUB":
             total += amount
     return total
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,27 @@
 import unittest
-from src.utils import read_transaction_from_file_json, get_transactions_rub_to_usd, sum_amount_from_transactions
+
+from src.utils import get_transactions_rub_to_usd, read_transaction_from_file_json, sum_amount_from_transactions
 
 
-def test_read_transaction_from_file_json():
-    transactions = read_transaction_from_file_json('path_to_your_test_file.json')
+def test_read_transaction_from_file_json() -> None:
+    """Test read transaction from file json"""
+    transactions = read_transaction_from_file_json("path_to_your_test_file.json")
     assert isinstance(transactions, list), "Должен вернуть список"
 
 
-def test_get_transactions_rub_to_usd():
-    rate = get_transactions_rub_to_usd('USD')
+def test_get_transactions_rub_to_usd() -> None:
+    """test get transactions rub to usd"""
+    rate = get_transactions_rub_to_usd("USD")
     assert isinstance(rate, float), "Должен вернуть число с плавающей точкой"
     assert rate > 0, "Курс должен быть больше нуля"
 
 
-def test_sum_amount_from_transactions():
+def test_sum_amount_from_transactions() -> None:
+    """test sum amount from transactions"""
     transactions = [
-        {'operationAmount': {'amount': '100', 'currency': {'code': 'USD'}}},
-        {'operationAmount': {'amount': '200', 'currency': {'code': 'EUR'}}},
-        {'operationAmount': {'amount': '300', 'currency': {'code': 'RUB'}}}
+        {"operationAmount": {"amount": "100", "currency": {"code": "USD"}}},
+        {"operationAmount": {"amount": "200", "currency": {"code": "EUR"}}},
+        {"operationAmount": {"amount": "300", "currency": {"code": "RUB"}}},
     ]
     total = sum_amount_from_transactions(transactions)
     assert isinstance(total, float), "Должен вернуть число с плавающей точкой"
@@ -29,5 +33,5 @@ test_get_transactions_rub_to_usd()
 test_sum_amount_from_transactions()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
